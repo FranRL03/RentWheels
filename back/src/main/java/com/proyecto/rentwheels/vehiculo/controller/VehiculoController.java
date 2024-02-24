21a5c919de0377cb6e17a881f3b248821ea1cf47
Fran Ruiz <fjavierrule@gmail.com>
2024-02-24 12:41:42 +0100

    creacion de entidad Modelo y ajustes en la base de datos

--- back/src/main/java/com/proyecto/rentwheels/vehiculo/controller/VehiculoController.java
+++ back/src/main/java/com/proyecto/rentwheels/vehiculo/controller/VehiculoController.java
@@ -3,6 +3,13 @@ package com.proyecto.rentwheels.vehiculo.controller;
 import com.proyecto.rentwheels.vehiculo.dto.GetFindAllDto;
 import com.proyecto.rentwheels.vehiculo.model.Vehiculo;
 import com.proyecto.rentwheels.vehiculo.service.VehiculoServicio;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,6 +27,40 @@ public class VehiculoController {
 
     private final VehiculoServicio vehiculoServicio;
 
+    @Operation(summary = "Lista de menu de todos los vehiculos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Lista de vehiculos",
+                    content = {@Content(mediaType = "aplication/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Vehiculo.class)),
+                            examples = {@ExampleObject(
+                                    value = """
+                                            [
+                                                     {
+                                                         "combustion": "ElÃ©ctrico",
+                                                         "modeloDto": {
+                                                             "modelo": "Kia Sportage"
+                                                         },
+                                                         "imagen": "https://assets.stickpng.com/thumbs/59db69d33752880e93e16efc.png",
+                                                         "transmision": "Manual"
+                                                     },
+                                                     {
+                                                         "combustion": "ElÃ©ctrico",
+                                                         "modeloDto": {
+                                                             "modelo": "Nissan Rogue"
+                                                         },
+                                                         "imagen": "https://assets.stickpng.com/thumbs/59db694b3752880e93e16eef.png",
+                                                         "transmision": "AutomÃ¡tico"
+                                                     },
+                                            ]
+                                            """
+                            )}
+                    )}),
+
+            @ApiResponse(responseCode = "500",
+                    description = "Error al mostrar la lista",
+                    content = @Content)
+    })
     @GetMapping("/menu")
     public Page<GetFindAllDto> findAll(@PageableDefault Pageable pageable){
 
