1910decfa9b36e92edfb61281e633664a15637c4
Fran Ruiz <fjavierrule@gmail.com>
2024-02-26 23:17:08 +0100

    diseño de datos

--- flutter_rent_car/lib/bloc/home_page/home_event.dart
+++ flutter_rent_car/lib/bloc/home_page/home_event.dart
@@ -2,3 +2,7 @@ part of 'home_bloc.dart';
 
 @immutable
 sealed class HomeEvent {}
+
+class DoHomeEvent extends HomeEvent{
+  DoHomeEvent();
+}
--- flutter_rent_car/lib/bloc/home_page/home_state.dart
+++ flutter_rent_car/lib/bloc/home_page/home_state.dart
@@ -4,3 +4,15 @@ part of 'home_bloc.dart';
 sealed class HomeState {}
 
 final class HomeInitial extends HomeState {}
+
+final class DoHomeLoading extends HomeState {}
+
+final class DoHomeSuccess extends HomeState {
+  final String successMessage;
+  DoHomeSuccess(this.successMessage);
+}
+
+final class DoHomeError extends HomeState {
+  final String errorMessage;
+  DoHomeError(this.errorMessage);
+}
