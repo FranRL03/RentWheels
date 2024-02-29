598cff74fed5c5eb561bebcaea2c4b78e17ff8db
Fran Ruiz <fjavierrule@gmail.com>
2024-02-29 17:54:45 +0100

    navigatorBar implementado

--- flutter_rent_car/lib/screen/page/home_page.dart
+++ flutter_rent_car/lib/screen/page/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_rent_car/bloc/user_details/user_bloc.dart';
 import 'package:flutter_rent_car/repositories/user/user_repository.dart';
 import 'package:flutter_rent_car/repositories/user/user_repository_impl.dart';
+import 'package:flutter_rent_car/screen/patatus.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,6 +13,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int currentPageIndex = 0;
   late UserRepository userRepository;
   late UserBloc _userBloc;
 
@@ -45,7 +47,39 @@ class _HomePageState extends State<HomePage> {
 
   Widget _buildHome() {
     return Scaffold(
-      body: Padding(
+      bottomNavigationBar: NavigationBar(
+        backgroundColor: const Color.fromARGB(255, 141, 186, 227),
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: const Color.fromARGB(255, 255, 255, 255),
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: '',
+          ),
+           NavigationDestination(
+            selectedIcon: Icon(Icons.car_rental_rounded),
+            icon: Icon(Icons.car_rental_outlined),
+            label: '',
+          ),
+           NavigationDestination(
+            selectedIcon: Icon(Icons.person),
+            icon: Icon(Icons.person_outline),
+            label: '',
+          ),
+        ],
+      ),
+      body: _buildPage(currentPageIndex)
+    );
+  }
+
+  Widget _page(){
+return Padding(
         padding: const EdgeInsets.only(top: 40, left: 30),
         child: Column(
           children: [
@@ -105,7 +139,19 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
-      ),
-    );
+      );
+  }
+
+  Widget _buildPage(int index){
+       switch (index) {
+    case 0:
+      return _page();
+    case 1:
+      return const MyWidget();
+    case 2:
+      return const MyWidget(); // Define la página de mensajes
+    default:
+      return Container(); // En caso de que el índice sea inválido
+  }
   }
 }
