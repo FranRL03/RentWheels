3fd3e49bc3725a645aecd46a035e8cb4f0f6f1d5
Fran Ruiz <fjavierrule@gmail.com>
2024-03-04 10:55:30 +0100

    conflicto resuelto

--- back/src/main/java/com/proyecto/rentwheels/alquiler/repository/AlquilerRepository.java
+++ back/src/main/java/com/proyecto/rentwheels/alquiler/repository/AlquilerRepository.java
@@ -1,10 +1,20 @@
 package com.proyecto.rentwheels.alquiler.repository;
 
+import com.proyecto.rentwheels.alquiler.dto.GetAlquileresCliente;
 import com.proyecto.rentwheels.alquiler.model.Alquiler;
-import com.proyecto.rentwheels.modelo.model.Modelo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface AlquilerRepository extends JpaRepository<Alquiler, UUID> {
+
+    @Query("""
+            select a
+            from Alquiler a
+            where a.cliente.id = :id
+            """)
+    Page<GetAlquileresCliente> getAllAlquileresCliente(Pageable pageable, UUID id);
 }
