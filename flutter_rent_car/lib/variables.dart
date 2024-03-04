25fc1ebd92b8d772c861abf30387ba25d616dae3
Fran Ruiz <fjavierrule@gmail.com>
2024-03-04 08:33:26 +0100

    cambios minimos

--- flutter_rent_car/lib/variables.dart
+++ flutter_rent_car/lib/variables.dart
@@ -2,4 +2,7 @@ import 'package:flutter/material.dart';
 
 String urlMovil = 'http://10.0.2.2:8080';
 String urlChrome = 'http://localhost:8080';
-Color colorPrincipal = const Color.fromRGBO(29, 47, 111, 1);
+
+class AppColors {
+  static Color colorPrincipal = const Color.fromRGBO(29, 47, 111, 1);
+}
