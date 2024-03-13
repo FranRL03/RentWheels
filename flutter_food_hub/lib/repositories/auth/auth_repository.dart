23d7d3f50644a522d683cc404de557885ae2734c
Fran Ruiz <fjavierrule@gmail.com>
2024-03-13 13:51:45 +0100

    duda

--- flutter_food_hub/lib/repositories/auth/auth_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_food_hub/model/dto/login_dto.dart';
-import 'package:flutter_food_hub/model/dto/register_dto.dart';
-import 'package:flutter_food_hub/model/response/login_response.dart';
-import 'package:flutter_food_hub/model/response/register_response.dart';
-
-abstract class AuthRepository {
-  Future<LoginResponse> login(LoginDto loginDto);
-  Future<RegisterResponse> register(RegisterDto registerDto);
-}
