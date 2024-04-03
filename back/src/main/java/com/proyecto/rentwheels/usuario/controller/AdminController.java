9e3229b8f6a1aa94822063f691e06e7ce6114704
Fran Ruiz <fjavierrule@gmail.com>
2024-04-03 16:44:39 +0200

    peticion de añadir en el back hecho

--- back/src/main/java/com/proyecto/rentwheels/usuario/controller/AdminController.java
+++ back/src/main/java/com/proyecto/rentwheels/usuario/controller/AdminController.java
@@ -1,5 +1,8 @@
 package com.proyecto.rentwheels.usuario.controller;
 
+import com.proyecto.rentwheels.usuario.service.AdminService;
+import com.proyecto.rentwheels.vehiculo.dto.EditVehiculoDto;
+import com.proyecto.rentwheels.vehiculo.dto.GetAllDetailsDto;
 import com.proyecto.rentwheels.vehiculo.dto.GetVehiculosDto;
 import com.proyecto.rentwheels.vehiculo.model.Vehiculo;
 import com.proyecto.rentwheels.vehiculo.service.VehiculoServicio;
@@ -14,15 +17,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
 
+    private final AdminService adminService;
     private final VehiculoServicio vehiculoServicio;
 
     @Operation(summary = "Lista de menu de todos los vehiculos")
@@ -65,6 +68,48 @@ public class AdminController {
         Page<Vehiculo> vehiculos = vehiculoServicio.findAll(pageable);
 
         return vehiculos.map(GetVehiculosDto::of);
+    }
+
+    @Operation(summary = "Añades un vehiculo")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Creación del vehiculo",
+                    content = {@Content(mediaType = "aplication/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Vehiculo.class)),
+                            examples = {@ExampleObject(
+                                    value = """
+                                            [
+                                                {
+                                                      "id": "ac19c001-8c06-178e-818c-0687df90000e",
+                                                      "nombre": "Picos",
+                                                      "imagen": "https://st.depositphotos.com/2078351/2100/i/450/depositphotos_21008703-stock-photo-a-bread-peaks-over-white.jpg",
+                                                      "descripcion": "picos",
+                                                      "precio": 0.13,
+                                                      "tags": [
+                                                          "pan",
+                                                          "integral"
+                                                      ],
+                                                      "categoria": {
+                                                          "nombre": "Tapas"
+                                                      }
+                                                  }
+                                            ]
+                                            """
+                            )}
+                    )}),
+
+            @ApiResponse(responseCode = "400",
+                    description = "Error al crear un vehiculo",
+                    content = @Content)
+    })
+    @PostMapping("/add/vehiculo")
+    public ResponseEntity<GetAllDetailsDto> create (@RequestBody EditVehiculoDto nuevo) {
+
+        Vehiculo v = adminService.createVehiculo(nuevo);
+
+        return ResponseEntity
+                .status(201)
+                .body(GetAllDetailsDto.of(v));
     }
 
 }
--- back/src/main/java/com/proyecto/rentwheels/usuario/service/AdminService.java
+++ back/src/main/java/com/proyecto/rentwheels/usuario/service/AdminService.java
@@ -1,9 +1,47 @@
 package com.proyecto.rentwheels.usuario.service;
 
+import com.proyecto.rentwheels.modelo.exception.NotFoundModeloException;
+import com.proyecto.rentwheels.modelo.model.Modelo;
+import com.proyecto.rentwheels.modelo.repositorio.ModeloRepository;
+import com.proyecto.rentwheels.vehiculo.dto.EditVehiculoDto;
+import com.proyecto.rentwheels.vehiculo.dto.GetAllDetailsDto;
+import com.proyecto.rentwheels.vehiculo.model.Vehiculo;
+import com.proyecto.rentwheels.vehiculo.repository.VehiculoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AdminService {
+
+    private final VehiculoRepository vehiculoRepository;
+    private final ModeloRepository modeloRepository;
+
+    public Vehiculo createVehiculo (EditVehiculoDto nuevoVehiculo) {
+
+        Vehiculo v = new Vehiculo();
+
+                v.setImagen(nuevoVehiculo.imagen());
+                v.setCombustible(nuevoVehiculo.combustion());
+                v.setTransmision(nuevoVehiculo.transmision());
+                v.setCapacidadPasajeros(nuevoVehiculo.capacidadPasajeros());
+                v.setAutonomia(nuevoVehiculo.autonomia());
+                v.setPotencia(nuevoVehiculo.potencia());
+                v.setEstado(nuevoVehiculo.estado());
+                v.setNumPuertas(nuevoVehiculo.numPuertas());
+                v.setDisponible(true);
+                v.setPrecioBase(nuevoVehiculo.precioBase());
+
+      Modelo modelo = modeloRepository.findByModeloIgnoreCase(nuevoVehiculo.modelo());
+
+        if (modelo == null)
+            throw new NotFoundModeloException();
+        else
+            v.setModelo(modelo);
+
+        return vehiculoRepository.save(v);
+    }
+
 }
